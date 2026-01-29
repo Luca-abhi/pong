314912c96306c487f7cb0b1c3085926327470697
Luca Abhi <2005ag1811@gmail.com>
2026-01-29 15:10:23 +0530

    added font,paddles and ball haluuum

--- main.lua
+++ main.lua
@@ -8,6 +8,15 @@ function love.load()
     
     love.graphics.setDefaultFilter("nearest","nearest")
 
+
+    smallfont=love.graphics.newFont("font.ttf",8)
+
+
+
+
+    love.graphics.setFont(smallfont)
+
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
         fullscreen =false,
         resizable=false,
@@ -21,13 +30,26 @@ function love.keypressed(key)
 end
 function love.draw()
     push:apply('start')
+
+
+
+    love.graphics.clear(40/255,45/255,52/255,255/255)
+
     love.graphics.printf(
         -- text,x,y,limit,align
         "HELLO PONG",
         0,
-        VIRTUAL_HEIGHT/2-6,
+        20,
         VIRTUAL_WIDTH,
         'center'
-)
+    )
+    -- two paddles
+
+    love.graphics.rectangle("fill",10,30,5,20)
+    love.graphics.rectangle("fill",VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-50,5,20)
+
+    -- ball
+
+    love.graphics.circle("fill",VIRTUAL_WIDTH/2,VIRTUAL_HEIGHT/2,4)
     push:apply('end')
 end
